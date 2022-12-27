5fa3b123f7e4a5fc5707e1a0c52fea9f5bf2bee8
Anastasia <Lari_an@bk.ru>
2022-12-27 23:44:53 +0300

    добавляю потерявшиеся файлы 9

---
+++ task9/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Задача 9
+// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+Console.Write("Введите первое число: ");
+Double FN = Double.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+Double SN = Double.Parse(Console.ReadLine()!);
+
+
+   if ( FN % SN == 0 )
+   {
+        Console.Write("Первое число кратно второму");
+   }
+   else
+   {
+    Console.WriteLine(FN/SN);
+    Console.WriteLine("Число не кратно");
+   }
