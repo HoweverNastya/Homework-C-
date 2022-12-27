be336d13001b739403366d4a55a3e618459a5990
Anastasia <Lari_an@bk.ru>
2022-12-27 23:43:18 +0300

    добавляю потерявшиеся файлы 7

---
+++ task7/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+//Задача 7
+//Дано число из отрезка [10, 99]. Показать наибольшую цифру числа
+
+int numberA = new Random().Next(10, 99);
+Console.WriteLine(numberA);
+
+if (numberA > 10 && numberA < 99)
+{
+if (numberA / 10 > numberA % 10)
+{
+        Console.WriteLine("Первая цифра больше");
+}
+else
+if (numberA / 10 == numberA % 10)
+{
+        Console.WriteLine("Цифры одинаковы");
+}
+else
+Console.WriteLine("Вторая цифра больше");
+}
